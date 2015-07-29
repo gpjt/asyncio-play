fa827c0487a2b71abf1c8a318f3e16a4fbf094e6
Giles Thomas <giles@pythonanywhere.com>
2015-07-29 18:46:30 +0100

    First cut at coroutine-based code to proxy to another server (just using www.resolversystems.com for now).

--- httpserver.py
+++ httpserver.py
@@ -15,42 +15,84 @@ class HTTPRequest(BaseHTTPRequestHandler):
         self.error_message = message
 
 
+
 @asyncio.coroutine
-def handle_request(reader, writer):
+def handle_request(downstream_reader, downstream_writer):
+    print("New connection")
     data = b""
 
-    while True:
-        try:
-            data += yield from asyncio.wait_for(reader.read(100), timeout=10)
-        except asyncio.TimeoutError:
-            print("TimeoutError!")
-            writer.write(b"Timeout!")
-            writer.close()
+    try:
+        while True:
+            try:
+                data += yield from asyncio.wait_for(downstream_reader.read(100), timeout=10)
+            except asyncio.TimeoutError:
+                print("TimeoutError!")
+                downstream_writer.write(b"Timeout!")
+                downstream_writer.close()
+                return
+
+            header_end_ix = data.find(b"\r\n\r\n")
+            if header_end_ix != -1:
+                print("Detected end of header")
+                break
+            print("Not at end of header yet")
+
+        request = HTTPRequest(data[:header_end_ix])
+        if request.error_code is not None:
+            downstream_writer.write("HTTP/1.0 {} error\n\n".format(request.error_code).encode("utf8"))
             return
 
-        header_end_ix = data.find(b"\r\n\r\n")
-        if header_end_ix != -1:
-            print("Detected end of header")
-            break
-        print("Not at end of header yet")
-
-    request = HTTPRequest(data[:header_end_ix])
-    if request.error_code is not None:
-        reply = "Error {}".format(request.error_code)
-    else:
         host = request.headers.get("Host")
         if host is None:
             print("It's a non-hosty one")
         else:
             print("It has the host {!r}".format(host))
 
-        reply = "You're trying to access {!r}".format(host)
+        print("Connecting upstream")
+        upstream_reader, upstream_writer = yield from asyncio.open_connection(
+            'www.resolversystems.com', 80, loop=loop
+        )
+        print("Got reader and writer for upstream, about to write {!r}".format(data))
+        upstream_writer.write(data)
+        print("Written buffer")
+        upstream_reader_task = asyncio.Task(upstream_reader.read(100))
+        downstream_reader_task = asyncio.Task(downstream_reader.read(100))
+        while True:
+            print("Waiting on both up- and down-stream readers")
+            done, pending = yield from asyncio.wait(
+                [upstream_reader_task, downstream_reader_task],
+                loop=loop,
+                return_when=asyncio.FIRST_COMPLETED
+            )
+            print("Got some data")
+            breakout = False
+            for future in done:
+                print("Result is {!r}".format(future.result()))
+                if future.result() is b"":
+                    breakout = True
 
-    print('Send: {!r}'.format(reply))
-    writer.write(reply.encode("utf8"))
+                if future == upstream_reader_task:
+                    print("It's the upstream reader.  Replacing it.")
+                    upstream_reader_task = asyncio.Task(upstream_reader.read(100))
+                    print("Writing to downstream")
+                    downstream_writer.write(future.result())
+                elif future == downstream_reader_task:
+                    print("It's the downstream reader.  Replacing it.")
+                    downstream_reader_task = asyncio.Task(downstream_reader.read(100))
+                    print("Writing to upstream")
+                    upstream_writer.write(future.result())
+                else:
+                    raise Exception("WTF?")
 
-    print('Close the client socket')
-    writer.close()
+            if breakout:
+                print("Result is none, breaking")
+                break
+
+
+    finally:
+        print('Close the client socket')
+        downstream_writer.close()
+
 
 loop = asyncio.get_event_loop()
 coro = asyncio.start_server(handle_request, '127.0.0.1', 8888, loop=loop)
@@ -67,4 +109,3 @@ except KeyboardInterrupt:
 server.close()
 loop.run_until_complete(server.wait_closed())
 loop.close()
-
